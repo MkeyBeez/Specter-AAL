02627ac054b833977eac81cee70d95671d3f68c6
MkeyBeez <statsdaddy69@gmail.com>
2025-09-10 16:17:06 +0200

    Update Setup-Win.bat

--- Setup-Win.bat
+++ Setup-Win.bat
@@ -11,7 +11,7 @@
 @echo "Installing..."
 
 :: Compile
-gcc -std=gnu99 -ansi -pedantic -O3 -Wno-int-conversion -c console.c aal.c specter.c benchmark.c
+gcc -std=gnu99 -pedantic -O3 -Wno-int-conversion -c console.c aal.c specter.c benchmark.c
 
 :: Link
 gcc -W -o Specter console.o aal.o specter.o benchmark.o
