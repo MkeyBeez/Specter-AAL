40aa606683096f69ce88c087424d7e518b14341b
MkeyBeez <statsdaddy69@gmail.com>
2025-09-10 15:43:28 +0200

    add updated headers aal.h

--- headers/aal.h
+++ headers/aal.h
@@ -1,11 +1,5 @@
-/******************************************************************************/
-/*                                   Specter                                  */
-/*                   <<Abstract Arithmetics Library Header>>                  */
-/*                              George Delaportas                             */
-/*                            Copyright © 2010-2025                           */
-/******************************************************************************/
-
-
+#ifndef AAL_H
+#define AAL_H
 
 /* Libraries */
 #include <stdio.h>
@@ -14,92 +8,57 @@
 #include <ctype.h>
 #include <string.h>
 
-/* AAL Header Tag */
-#define __AAL_H__ 		1
-
 /* Fixate Numbers Length - Result */
-typedef struct fixlenres
-{
-	char Bigger;
-	char *Num1;
-	char *Num2;
-	uintptr_t FinLen;
+typedef struct {
+    char Bigger;        /* '0' if equal length, '1' if A bigger, '2' if B bigger */
+    char *Num1;         /* Adjusted first number string */
+    char *Num2;         /* Adjusted second number string */
+    uintptr_t FinLen;   /* Final length after padding */
 } fixlen;
 
 /* Read File - Output */
-typedef struct rdfloutres
-{
-	char *Num1;
-	char *Num2;
+typedef struct {
+    char *Num1;
+    char *Num2;
 } rdflout;
 
-/* AAL - Length */
-uintptr_t aal_len(char *X);
+/* Core string utilities */
+uintptr_t aal_len(const char *X);
+char     *aal_copy(const char *S, unsigned long P);
+char      aal_cmp(const char *A, const char *B);
+char     *aal_rvrs(const char *X);
+char     *aal_pad(const char *X, const char *S);
 
-/* AAL - Copy */
-char *aal_copy(char *S, unsigned long P);
+/* Numeric checks */
+char      aal_zrchk(const char *X);
+char     *aal_clrizr(const char *X);
+char      aal_minchk(const char *X);
+char     *aal_setmin(const char *X);
+char     *aal_clrmin(const char *X);
 
-/* AAL - Compare */
-char aal_cmp(char *A, char *B);
+/* Dot (decimal) utilities */
+uintptr_t aal_dotchk(const char *X);
+char     *aal_setdot(const char *X, size_t pos);
+char     *aal_clrdot(const char *X);
 
-/* AAL - Reverse */
-char *aal_rvrs(char *X);
+/* Alignment helpers */
+fixlen    aal_fixlen(const char *A, const char *B);
 
-/* AAL - Padding */
-char *aal_pad(char *X, char *S);
+/* File input */
+rdflout   aal_rdfl(const char *Z);
 
-/* AAL - Zero Check */
-char aal_zrchk(char *X);
+/* Error checking */
+char      aal_errchk_1(const char *X);
+char      aal_errchk_2(const char *A, const char *B);
 
-/* AAL - Clear Initial Zeroes */
-char *aal_clrizr(char *X);
+/* Memory management */
+char     *aal_mem_alloc_num(size_t V);
+char     *aal_mem_alloc_1(const char *A);
+char     *aal_mem_alloc_2(const char *A, const char *B);
+int       aal_mem_dealloc(char *S);
 
-/* AAL - Minus Check (Minus Sign) */
-char aal_minchk(char *X);
+/* Arithmetic */
+char     *aal_add(const char *A, const char *B);
+char     *aal_sub(const char *A, const char *B);
 
-/* AAL - Set Minus Sign */
-char *aal_setmin(char *X);
-
-/* AAL - Clear Minus Sign */
-char *aal_clrmin(char *X);
-
-/* AAL - Dot Check (Comma Sign) */
-uintptr_t aal_dotchk(char *X);
-
-/* AAL - Set Dot (Comma Sign) */
-char *aal_setdot(char *X, char *P);
-
-/* AAL - Clear Dot (Comma Sign) */
-char *aal_clrdot(char *X);
-
-/* AAL - Fixate Numbers Length */
-fixlen aal_fixlen(char *A, char *B);
-
-/* AAL - Read File */
-rdflout aal_rdfl(const char *Z);
-
-/* AAL - Error Check (1 Argument) */
-char aal_errchk_1(char *X);
-
-/* AAL - Error Check (2 Arguments) */
-char aal_errchk_2(char *A, char *B);
-
-/* AAL - Memory Allocator (1 Argument) */
-char *aal_mem_alloc_num(unsigned long V);
-
-/* AAL - Memory Allocator (1 Argument) */
-char *aal_mem_alloc_1(char *A);
-
-/* AAL - Memory Allocator (2 Arguments) */
-char *aal_mem_alloc_2(char *A, char *B);
-
-/* AAL - Memory Deallocator */
-int aal_mem_dealloc(char *S);
-
-/* AAL - Addition */
-char *aal_add(char *A, char *B);
-
-/* AAL - Subtraction */
-char *aal_sub(char *A, char *B);
-
-/******************************************************************************/
+#endif /* AAL_H */
