3a1e33763c4f160a50c11004852d6c6ca821a2a8
MkeyBeez <statsdaddy69@gmail.com>
2025-09-11 22:42:48 +0200

    Update aal.h

--- headers/aal.h
+++ headers/aal.h
@@ -18,13 +18,20 @@
 #include <stdbool.h>
 #include <stddef.h>
 
+/* Comparison result enum */
+typedef enum {
+    CMP_EQUAL = 0,
+    CMP_A_GREATER = 1, 
+    CMP_B_GREATER = 2
+} aal_cmp_result;
+
 /* Fixate Numbers Length - Result */
 typedef struct fixlenres
 {
-    char Bigger;
-    char *Num1;          /* Changed from const char* to char* since they may be allocated */
-    char *Num2;          /* Changed from const char* to char* since they may be allocated */
-    uintptr_t FinLen;    /* Changed from size_t to uintptr_t for consistency */
+    char bigger;         /* Updated field name */
+    char *num1;          /* Updated field name, changed from const char* to char* since they may be allocated */
+    char *num2;          /* Updated field name, changed from const char* to char* since they may be allocated */
+    uintptr_t finalLen;  /* Updated field name, changed from size_t to uintptr_t for consistency */
 } fixlen;
 
 /* Read File - Output */
@@ -34,21 +41,6 @@ typedef struct rdfloutres
     char *Num2;
 } rdflout;
 
-// Updated function declarations
-bool aal_zrchk(char *X);
-bool aal_minchk(char *X);  
-bool aal_errchk_1(char *X);
-bool aal_errchk_2(char *A, char *B);
-
-// New comparison enum and function
-typedef enum {
-    CMP_EQUAL = 0,
-    CMP_A_GREATER = 1, 
-    CMP_B_GREATER = 2
-} aal_cmp_result;
-
-aal_cmp_result aal_cmp(char *A, char *B);
-
 /* Function Declarations */
 
 /* AAL - Length */
@@ -57,8 +49,8 @@ uintptr_t aal_len(char *X);
 /* AAL - Copy */
 char *aal_copy(char *S, unsigned long P);
 
-/* AAL - Compare */
-char aal_cmp(char *A, char *B);
+/* AAL - Compare - Updated to return aal_cmp_result */
+aal_cmp_result aal_cmp(char *A, char *B);
 
 /* AAL - Reverse */
 char *aal_rvrs(char *X);
@@ -66,14 +58,14 @@ char *aal_rvrs(char *X);
 /* AAL - Padding */
 char *aal_pad(char *X, char *S);
 
-/* AAL - Zero Check */
-char aal_zrchk(char *X);
+/* AAL - Zero Check - Updated to return bool */
+bool aal_zrchk(char *X);
 
 /* AAL - Clear Initial Zeroes */
 char *aal_clrizr(char *X);
 
-/* AAL - Minus Check (Minus Sign) */
-char aal_minchk(char *X);
+/* AAL - Minus Check (Minus Sign) - Updated to return bool */
+bool aal_minchk(char *X);
 
 /* AAL - Set Minus Sign */
 char *aal_setmin(char *X);
@@ -96,11 +88,11 @@ fixlen aal_fixlen(char *A, char *B);
 /* AAL - Read File */
 rdflout aal_rdfl(const char *Z);
 
-/* AAL - Error Check (1 Argument) */
-char aal_errchk_1(char *X);
+/* AAL - Error Check (1 Argument) - Updated to return bool */
+bool aal_errchk_1(char *X);
 
-/* AAL - Error Check (2 Arguments) */
-char aal_errchk_2(char *A, char *B);
+/* AAL - Error Check (2 Arguments) - Updated to return bool */
+bool aal_errchk_2(char *A, char *B);
 
 /* AAL - Memory Allocator (Numeric) */
 char *aal_mem_alloc_num(unsigned long V);
@@ -119,6 +111,11 @@ char *aal_add(char *A, char *B);
 
 /* AAL - Subtraction */
 char *aal_sub(char *A, char *B);
+
+/* Helper function declarations for decimal arithmetic */
+char *aal_add_integers(char *A, char *B);
+char *normalize_for_decimal_arithmetic(char *num, uintptr_t targetDecimalPlaces, bool hasDecimal);
+char *insert_decimal_point(char *intResult, uintptr_t decimalPlaces);
 
 #endif /* __AAL_H__ */
 /******************************************************************************/
